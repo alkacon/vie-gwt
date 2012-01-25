bdc99f249bf39f69dacd6c71f55780b02a2fcbd0
tHerrmann <t.herrmann@alkacon.com>
2012-01-25 18:16:45 +0100

    Added entity event handling test.

--- test/com/alkacon/vie/client/VieTest.java
+++ test/com/alkacon/vie/client/VieTest.java
@@ -29,12 +29,26 @@ package com.alkacon.vie.client;
 
 import java.util.List;
 
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.junit.client.GWTTestCase;
 
 /**
  * Tests for {@link Vie}.<p>
  */
 public class VieTest extends GWTTestCase {
+
+    /** The complex type attribute name. */
+    public static final String ATTRIBUTE_NAME = "<http:opencms/simpleAttribute>";
+
+    /** The complex type name. */
+    public static final String COMPLEX_TYPE_ID = "<cms:complex>";
+
+    /** The simple type name. */
+    public static final String SIMPLE_TYPE_ID = "<cms:simple>";
+
+    /** Change counter. */
+    private int m_changeCount;
 
     /**
      * @see com.google.gwt.junit.client.GWTTestCase#getModuleName()
@@ -76,28 +90,66 @@ public class VieTest extends GWTTestCase {
     /**
      * Tests the creation of new type attributes.<p>
      */
-    public void testTypeAttribute() {
+    public void testEntityAttribute() {
 
-        String simpleTypeId = "<cms:simple>";
-        String complexTypeId = "<cms:complex>";
-        String attributeName = "<http:opencms/simpleAttribute>";
         I_Vie vie = getVieInstance();
 
-        I_Type simple = vie.createType(simpleTypeId);
-        I_Type complex = vie.createType(complexTypeId);
-        complex.addAttribute(attributeName, simpleTypeId, 1, 1);
+        I_Type simple = vie.createType(SIMPLE_TYPE_ID);
+        I_Type complex = vie.createType(COMPLEX_TYPE_ID);
+        complex.addAttribute(ATTRIBUTE_NAME, SIMPLE_TYPE_ID, 1, 1);
         assertNotNull("The newly created type should not be null", simple);
         assertNotNull("The newly created type should not be null", complex);
-        assertEquals("The type id should match the initial id", simpleTypeId, simple.getId());
-        assertEquals("The type id should match the initial id", complexTypeId, complex.getId());
+        assertEquals("The type id should match the initial id", SIMPLE_TYPE_ID, simple.getId());
+        assertEquals("The type id should match the initial id", COMPLEX_TYPE_ID, complex.getId());
         assertTrue("This should be a simple type, as it has no attributes.", simple.isSimpleType());
         assertFalse("This should be no simple type, as it has attributes.", complex.isSimpleType());
         assertNotNull("The attribute list should not be null.", complex.getAttributeNames());
         assertEquals("The attribute names list should have a length of 1.", 1, complex.getAttributeNames().size());
-        String attributeType = complex.getAttributeTypeName(attributeName);
-        assertEquals("The attribute should be of the type: " + simpleTypeId, simpleTypeId, attributeType);
+        String attributeType = complex.getAttributeTypeName(ATTRIBUTE_NAME);
+        assertEquals("The attribute should be of the type: " + SIMPLE_TYPE_ID, SIMPLE_TYPE_ID, attributeType);
         List<String> attributes = complex.getAttributeNames();
-        assertEquals("The attribute name should be as given above.", attributeName, attributes.get(0));
+        assertEquals("The attribute name should be as given above.", ATTRIBUTE_NAME, attributes.get(0));
+    }
+
+    /**
+     * Tests the event handling on entities.<p>
+     * Relies on the types created in {@link #testEntityAttribute()}.<p>
+     */
+    public void testEntityEvents() {
+
+        I_Vie vie = getVieInstance();
+        I_Entity entity = vie.createEntity("myEntity", COMPLEX_TYPE_ID);
+        final String changeValue = "my new value";
+        resetChangeCount();
+        ((Entity)entity).addValueChangeHandler(new ValueChangeHandler<I_Entity>() {
+
+            public void onValueChange(ValueChangeEvent<I_Entity> event) {
+
+                incrementChangeCount();
+                String attributeValue = event.getValue().getAttribute(ATTRIBUTE_NAME).getSimpleValue();
+                assertEquals(changeValue, attributeValue);
+            }
+        });
+        entity.setAttributeValue(ATTRIBUTE_NAME, changeValue);
+        assertEquals(1, getChangeCount());
+    }
+
+    /**
+     * Returns the change count.<p>
+     * 
+     * @return the change count
+     */
+    protected int getChangeCount() {
+
+        return m_changeCount;
+    }
+
+    /**
+     * Increments the change counter.<p>
+     */
+    protected void incrementChangeCount() {
+
+        m_changeCount++;
     }
 
     /**
@@ -108,6 +160,14 @@ public class VieTest extends GWTTestCase {
     private I_Vie getVieInstance() {
 
         return Vie.getInstance();
+    }
+
+    /**
+     * Resets the change counter.<p>
+     */
+    private void resetChangeCount() {
+
+        m_changeCount = 0;
     }
 
 }
